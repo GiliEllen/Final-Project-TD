1e107b6b1140ebe9a79021db7886943b36187c70
gili <gilimena95@gmail.com>
2025-01-14 20:28:36 +0200

    wip: ball logic

--- Assets/Scripts/Toys/Ball/Ball.cs
+++ Assets/Scripts/Toys/Ball/Ball.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : Toy
 {
-    // Constructor (if used without Unity MonoBehaviour lifecycle)
+    public float speed = 5f;  
+    private Rigidbody rb;
+
     public Ball()
     {
         hp = 5;
@@ -18,29 +18,21 @@ public class Ball : Toy
         gridHeight = 2;
     }
 
-    // // Use Unity's Start method for initialization
-    // void Start()
-    // {
-    //     hp = 5;
-    //     isMovable = true;
-    //     isMoving = false;
-    //     canBulletBounce = true;
-    //     shotDegree = 0;
-    //     shotAmount = 0;
-    //     manaCost = 3;
-    //     gridWidth = 2;
-    //     gridHeight = 2;
-    // }
+    void Start() {
+        StartMove();
+    }
 
     public void StartMove()
     {
-        // TODO: Calculate trajectory
         isMoving = true;
         MoveBall();
     }
 
     public void MoveBall()
     {
-        // TODO: Implement ball movement logic
+        rb = GetComponent<Rigidbody>();
+
+        Vector3 trajectory = BallTrajectory.CalculateTrajectory(GameObject.Find("center"), transform);
+        rb.velocity = trajectory * speed;
     }
 }
