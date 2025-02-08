7ee3436d8fadb158f6071098c7c623993f34c6d3
gili <gilimena95@gmail.com>
2025-02-08 18:21:37 +0200

    wip: small fixes to scene and build

--- Assets/Scripts/Level/NewLevelManager.cs
+++ Assets/Scripts/Level/NewLevelManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
