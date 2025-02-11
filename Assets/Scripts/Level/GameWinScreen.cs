73217db8da266cc60a093c60b4d7529ff8c5da3b
gili <gilimena95@gmail.com>
2025-02-11 21:51:19 +0200

    remove pause

--- Assets/Scripts/Level/GameWinScreen.cs
+++ Assets/Scripts/Level/GameWinScreen.cs
@@ -27,11 +27,6 @@ public class GameWinScreen : MonoBehaviour
 
     private void ToggleActiveStatus(bool status) {
         gameObject.SetActive(status);
-        if (status) {
-             Time.timeScale = 0f;
-        } else {
-            Time.timeScale = 1f;          
-        }
     }
 
 }
